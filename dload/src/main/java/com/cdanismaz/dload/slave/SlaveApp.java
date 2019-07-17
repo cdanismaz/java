f03224e6caed817eb1ab6bace102547a2d12fc55
Furkan Kürşat Danışmaz <danismaz.furkan@gmail.com>
2019-07-17 07:36:31 +0300

    Removed unused imports

--- dload/src/main/java/com/cdanismaz/dload/slave/SlaveApp.java
+++ dload/src/main/java/com/cdanismaz/dload/slave/SlaveApp.java
@@ -1,14 +1,10 @@
 package com.cdanismaz.dload.slave;
 
 import com.cdanismaz.dload.common.ThreadManager;
-import com.cdanismaz.dload.master.MasterTcpManager;
-
-import java.io.*;
-import java.net.*;
 
 public class SlaveApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ThreadManager threadManager = new ThreadManager();
 
         SlaveInputListener slaveInputListener = new SlaveInputListener(threadManager);
